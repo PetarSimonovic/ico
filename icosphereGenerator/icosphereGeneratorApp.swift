c91c3b0b783da9b0c2efdd6823e0858a8d863786
PetarSimonovic <petar@simonovic.com>
2023-05-04 15:07:38 +0100

    Initial Commit

---
+++ icosphereGenerator/icosphereGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  icosphereGeneratorApp.swift
+//  icosphereGenerator
+//
+//  Created by Petar Simonovic on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct icosphereGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
